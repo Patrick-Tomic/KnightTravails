fa7e244975cc64717f138a8048fd57d4a7fcbb4b
Patrick-Tomic <pat12tomic@gmail.com>
2023-12-12 06:29:49 -0500

    found pathway, ending found

--- src/knight.ts
+++ src/knight.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/restrict-plus-operands */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
@@ -9,6 +10,7 @@ export default class Game {
   moves: number
   positions: number[][]
   queue: number[][]
+  found: boolean
   constructor (start: number[], end: number[]) {
     this.node = new Node(start)
     this.ending = end
@@ -22,28 +24,40 @@ export default class Game {
       [1, 2],
       [-1, 2]
     ]
-    this.moves = 1
-    this.queue = []
+    this.moves = 0
+    this.queue = [start]
+    this.found = false
   }
 
   nextarr (Position: number[]) {
     const newArr: number[][] = this.positions.map((position) => [position[0] + Position[0], position[1] + Position[1]])
-    console.log(newArr)
     while (newArr.length) {
+      if (newArr[0][0] === this.ending[0] && newArr[0][1] === this.ending[1]) {
+        console.log('FOUND IT!!!')
+        this.found = true
+        this.queue.push(newArr[0])
+        console.log(this.queue.length)
+        return
+      }
       if (newArr[0][0] >= 0 && newArr[0][0] <= 7 && newArr[0][1] >= 0 && newArr[0][1] <= 7) {
         this.queue.push(newArr[0])
-        console.log(newArr[0])
         newArr.shift()
       } else {
         newArr.shift()
       }
     }
+    this.moves++
+    // eslint-disable-next-line array-callback-return
   }
 
   findEnd () {
-    this.queue.push(this.node.position)
+    if (this.found) {
+      return
+    }
+    console.log(this.queue[0] + '=' + this.ending)
     this.nextarr(this.queue[0])
     this.queue.shift()
-    console.log(this.queue.length)
+    this.findEnd()
+    console.log(this.queue)
   }
 }
